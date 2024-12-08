5b03660b4d544feb5e114f403bfa5fd21386af90
42arslanyusuf <42arslan.yusuf@gmail.com>
2024-12-08 21:03:39 +0300

    diagonal difference

---
+++ src/Warmup/_005_DiagonalDifference/Main.java
@@ -0,0 +1,27 @@
+package Warmup._005_DiagonalDifference;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+        int leftToRightDiagonal = 0;
+        int rightToLeftDiagonal = 0;
+        int result = 0;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            leftToRightDiagonal += arr.get(i).get(i);
+        }
+        
+        for (int i = 0; i < arr.size(); i++) {
+            rightToLeftDiagonal += arr.get(i).get(arr.size() - i - 1);
+        }
+        
+        result = Math.abs(leftToRightDiagonal - rightToLeftDiagonal);
+        return result;
+    }
+}
