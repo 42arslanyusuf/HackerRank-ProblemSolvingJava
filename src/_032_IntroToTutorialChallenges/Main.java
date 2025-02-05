bb801e1f0bbad106c4f904e85f256f23cb49af01
42arslan.yusuf <42arslan.yusuf@gmail.com>
2025-02-05 11:43:16 +0300

    intro to tutorial challenges

---
+++ src/_032_IntroToTutorialChallenges/Main.java
@@ -0,0 +1,13 @@
+package _032_IntroToTutorialChallenges;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static int introTutorial(int V, List<Integer> arr) {
+        // Write your code here
+        return arr.indexOf(V);
+    }
+}
